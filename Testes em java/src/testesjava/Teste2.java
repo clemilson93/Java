ff2c493ed221692134863a81f7ab68214f62022c
clemilson93 <81972737+clemilson93@users.noreply.github.com>
2022-05-13 13:30:23 -0300

    Add files via upload

---
+++ Testes em java/src/testesjava/Teste2.java
@@ -0,0 +1,31 @@
+package testesjava;
+
+public class Teste2 {
+
+	public static void main(String[] args) {
+		System.out.println("Contador principal: " + Contador.contador);
+		Contador contador1 = new Contador();
+		System.out.println("Contador 1: " + contador1.contador);
+		Contador contador2 = new Contador();
+		System.out.println("Contador 2: " + contador2.contador);
+		Contador contador3 = new Contador();
+		System.out.println("Contador 3: " + contador3.contador);
+		System.out.println("Contador principal: " + Contador.contador);
+		Contador.contador = 0;
+		System.out.println("Contador 1: " + contador1.contador);
+		System.out.println("Contador 2: " + contador2.contador);
+		System.out.println("Contador 3: " + contador3.contador);
+		System.out.println("Contador principal: " + Contador.contador);
+
+	}
+
+}
+
+class Contador{
+	
+	static int contador;
+	
+	Contador(){
+		contador++;
+	}
+}
